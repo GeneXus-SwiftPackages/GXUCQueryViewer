8e3da8cf667e8d132845ecd034283546ff395cb0
Build Er <builder@macmini6.local>
2023-05-05 10:31:44 -0300

    Package version: 1.0.0-beta+20230505132240

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCQueryViewer",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCQueryViewer-1.0.0-beta+20230505123704.xcframework.zip",
-			checksum: "a91c8b7a996caf36b07718986176cb0d3cca7d80c1fc5c8ddb0b32948fa1f85e"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCQueryViewer-1.0.0-beta+20230505132240.xcframework.zip",
+			checksum: "e1b5ffd276d21fac4caa18e2ecda530ba72b386d8ac162bc4d4f47b8d813f26b"
 		)
 	]
 )
