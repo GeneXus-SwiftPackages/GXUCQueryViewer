56cc6cf2e14dedad3f81865bf4bfd6607828e1de
genexusbot <builder@genexus.com>
2023-05-15 14:55:24 -0300

    Package version: 1.0.0-beta+20230515174624

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCQueryViewer",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCQueryViewer-1.0.0-beta+20230515153434.xcframework.zip",
-			checksum: "709dacef6a1198e05c7d1fde552f3a18e67e3d3bc24b51bd5fde3a8010fb7df5"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCQueryViewer-1.0.0-beta+20230515174624.xcframework.zip",
+			checksum: "1352c86160847ae290cd1fc83db8ebe24ab21175f8288681803cbec046357547"
 		)
 	]
 )
