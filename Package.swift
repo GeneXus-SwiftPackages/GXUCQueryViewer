2bf7feafc6f2ab988f6711a3b6a5f0fc101525c2
genexusbot <builder@genexus.com>
2023-07-29 18:46:31 -0300

    Package version: 1.1.0-rc.1

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXUCQueryViewer",
+	platforms: [.iOS("12.0")],
+	products: [
+		.library(
+			name: "GXUCQueryViewer",
+			targets: ["GXUCQueryViewerWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0-rc.1")
+	],
+	targets: [
+		.target(name: "GXUCQueryViewerWrapper",
+				dependencies: [
+					"GXUCQueryViewer",
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXUCQueryViewer",
+			url: "https://pkgs.genexus.dev/iOS/releases/GXUCQueryViewer-1.1.0-rc.1.xcframework.zip",
+			checksum: "69b910af584b9318fa7ab765e6bd6e011aff1bb93d580769e2bf77bd5f516c2d"
+		)
+	]
+)
