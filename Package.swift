d2bcb5acd352a3f05d805abdfad094722fa96a2f
Build Er <builder@macmini6.local>
2024-05-14 06:25:37 -0300

    Package version: 2.0.0-beta.32

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXUCQueryViewerWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.0-beta.31")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.0-beta.32")
 	],
 	targets: [
 		.target(name: "GXUCQueryViewerWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCQueryViewer",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCQueryViewer-2.0.0-beta.31.xcframework.zip",
-			checksum: "67531b08603b51801292be5c4648fa29aa4ff0e675f7944f1a46206b17e1a2f8"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCQueryViewer-2.0.0-beta.32.xcframework.zip",
+			checksum: "e720889d9c65c3014e8e605f33ada5cb228636ea1fe97e3285083670245d1350"
 		)
 	]
 )
