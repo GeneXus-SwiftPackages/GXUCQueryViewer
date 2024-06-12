f53c3325612b81d6196bf8ce9e493bcd5e603883
Build Er <builder@macmini6.local>
2024-06-12 14:23:39 -0300

    Package version: 2.0.12

--- Package.swift
+++ Package.swift
@@ -1,28 +1,28 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
 	name: "GXUCQueryViewer",
-	platforms: [.iOS("12.0")],
+	platforms: [.iOS("12.0"), .visionOS("1.0")],
 	products: [
 		.library(
 			name: "GXUCQueryViewer",
 			targets: ["GXUCQueryViewerWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.12")
 	],
 	targets: [
 		.target(name: "GXUCQueryViewerWrapper",
 				dependencies: [
 					"GXUCQueryViewer",
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .visionOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCQueryViewer",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXUCQueryViewer-1.1.0.xcframework.zip",
-			checksum: "ad0caf5ff0ade7471badb67002f959fe7321fd11d16db636e839c386ecd524ab"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXUCQueryViewer-2.0.12.xcframework.zip",
+			checksum: "39819c7bd6a9a650605f05b85f6f77184869e84f3f94deedbae1ea18add58a35"
 		)
 	]
 )
